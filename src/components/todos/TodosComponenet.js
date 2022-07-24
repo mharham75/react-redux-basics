55bed146cdae449b6a7f1c3baab3c1512e25e8d4
Md Arham <md.arham@globallogic.com>
2022-07-24 16:05:38 +0530

    undisplay buttons

--- src/components/todos/TodosComponenet.js
+++ src/components/todos/TodosComponenet.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { connect } from 'react-redux'
 import { fetchTodos } from '../../redux/todos/TodosAction'
 
 const TodosComponenet = ({todos, fetchTodos}) => {
+
+    const [isTodos,setIsTodos] = useState(false)
+
+    const getTodos = () => {
+        fetchTodos()
+        setIsTodos(true)
+    }
+
   return (
     <>
-        <button onClick={fetchTodos}>Click to Get Your Todos</button>
+        {!isTodos && <button onClick={getTodos}>Click to Get Your Todos</button>}
         {todos && todos.map(todo => (
             <div key={todo.id}>
                 <h2>Title : {todo.title}</h2>
--- src/components/user/UserComponent.js
+++ src/components/user/UserComponent.js
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { connect } from 'react-redux'
 import { fetchUsers } from '../../redux/user/UserActions'
 
 import "./UserStyle.css"
 
 const UserComponent = ({ users,fetchUsers }) => {
+
+    const[isUsers,setIsUsers] = useState(false)
+
+    const getUsers = () => {
+        fetchUsers()
+        setIsUsers(true)
+    }
+
   return (
     <>
-        <button onClick={ () => fetchUsers()}>Click To Get List Of Users</button>
+        {!isUsers && <button onClick={getUsers}>Click To Get List Of Users</button>}
+        {users.loading && <p>Loading...</p>}
+        {users.error && <p>{users.error}</p>}
         {users && users.map( u =>(
             <div className="user-card" key={u.id}>
                 <h2 className='user-card-heading'>{u.username}</h2>
